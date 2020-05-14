ed70b1e8552fb407524fb8a24a24fb7541455e23
imjustpassby <260809970@qq.com>
2020-05-14 17:41:21 +0800

    feat(src & examples): create an easy axios & build express to test

---
+++ src/types/index.ts
@@ -0,0 +1,19 @@
+export type Method =
+  | 'get'
+  | 'GET'
+  | 'delete'
+  | 'DELETE'
+  | 'head'
+  | 'HEAD'
+  | 'post'
+  | 'POST'
+  | 'put'
+  | 'PUT'
+  | 'patch'
+  | 'PATCH'
+export interface AxiosRequestConfig {
+  url: string
+  method?: Method
+  data?: any
+  params?: any
+}
