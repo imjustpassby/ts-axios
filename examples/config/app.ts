5e26e5500efebb521042bd091d5ee50f1181f662
imjustpassby <260809970@qq.com>
2020-05-20 16:44:50 +0800

    feat(transform): transform

--- examples/config/app.ts
+++ examples/config/app.ts
@@ -1,9 +1,9 @@
-import axios from '../../src/index'
+import axios, { AxiosTransformer } from '../../src/index'
 import qs from 'qs'
 
 axios.defaults.headers.common['test2'] = 132
 
-axios({
+/* axios({
   url: '/config/post',
   method: 'post',
   data: qs.stringify({
@@ -11,6 +11,37 @@ axios({
   }),
   headers: {
     test1: '333'
+  }
+}).then(res => {
+  console.log(res.data)
+}) */
+
+axios({
+  transformRequest: [
+    /* 
+    request.data处理逻辑
+    */
+    function(data) {
+      return qs.stringify(data)
+    },
+    ...(axios.defaults.transformRequest as AxiosTransformer[])
+  ],
+  transformResponse: [
+    ...(axios.defaults.transformResponse as AxiosTransformer[]),
+    function(data) {
+      /* 
+      response.data 处理逻辑
+      */
+      /* if (typeof data === 'object') {
+        data.b = 2
+      } */
+      return data
+    }
+  ],
+  url: '/config/post',
+  method: 'post',
+  data: {
+    a: '/config/post'
   }
 }).then(res => {
   console.log(res.data)
